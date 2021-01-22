c667c28e2932f3de1d08363b9aff4706d5bcab3c
Matthew <mutee88@gmail.com>
2021-01-22 15:38:49 +0300

    Add Daemon skeleton complete with DI and logging to file

---
+++ omniscraper/Omniscraper.Daemon/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Omniscraper.Daemon
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddJsonFile("appsettings.json", false);
+
+            })
+            .ConfigureLogging((context, builder) =>
+            {
+                builder.AddConsole((log) =>
+                {
+
+                });
+
+                Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("process", "omniscraper.daemon")
+                .WriteTo
+                .File("log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+                builder.AddSerilog(Log.Logger);
+
+            }).ConfigureServices(ConfigureServices);
+
+        static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.AddHostedService<TweetListeningBackgroundService>();
+
+            services.AddLogging();
+            
+        }
+
+    }
+}
---
+++ omniscraper/Omniscraper.Daemon/TweetListeningBackgroundService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Omniscraper.Daemon
+{
+    public class TweetListeningBackgroundService : BackgroundService
+    {
+        ILogger<TweetListeningBackgroundService> logger;
+        public TweetListeningBackgroundService(ILogger<TweetListeningBackgroundService> logger)
+        {
+            this.logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.FromResult(1);
+                logger.LogInformation("Running at this time {}", DateTime.UtcNow);
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+            }
+        }
+    }
+}
