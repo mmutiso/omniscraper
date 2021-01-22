c8b5dcae6fc424928149cbdb97bd7fbc88804521
Matthew <mutee88@gmail.com>
2021-01-22 20:19:22 +0300

    Remove testing data from driver class

--- omniscraper/Omniscraper.Sample/Program.cs
+++ omniscraper/Omniscraper.Sample/Program.cs
@@ -9,12 +9,40 @@ using Newtonsoft.Json;
 using Omniscraper.Core.TwitterScraper.Entities;
 using Omniscraper.Core.TwitterScraper;
 using Omniscraper.Core;
+using System.Diagnostics;
 
 namespace Omniscraper.Sample
 {
     class Program
     {
         static async Task Main(string[] args)
+        {
+            await StreamMain();
+
+            //await AuthSampleAsync();
+        }
+
+        static async Task AuthSampleAsync()
+        {
+            ILoadApplicationCredentials credentialsLoader = new EnvironmentVariablesKeysLoader();
+            TwitterKeys keys = credentialsLoader.Load();
+
+            var auth = new SingleUserAuthorizer
+            {
+                CredentialStore = new SingleUserInMemoryCredentialStore
+                {
+                    ConsumerKey = keys.ConsumerKey,
+                    ConsumerSecret = keys.ConsumerSecret,
+                    AccessToken = "",
+                    AccessTokenSecret = ""
+                }
+            };
+
+            var ctx = new TwitterContext(auth);
+            await ctx.TweetAsync("Just another day, testing");
+        }
+
+        static async Task StreamMain()
         {
             ILoadApplicationCredentials credentialsLoader = new EnvironmentVariablesKeysLoader();
             TwitterKeys keys = credentialsLoader.Load();
@@ -29,25 +57,25 @@ namespace Omniscraper.Sample
             CancellationTokenSource tokenSource = new CancellationTokenSource();
 
             IQueryable<Streaming> twitterStream = context.CreateStream(keywords, tokenSource.Token);
-            Console.WriteLine("Passed stream creation");            
-            Task task = twitterStream.StartAsync((content) => Task.Factory.StartNew(() => 
-             {
-                 Console.WriteLine(content.Content);
+            Console.WriteLine("Passed stream creation");
+            Task task = twitterStream.StartAsync((content) => Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine(content.Content);
 
-                 if (content.Content.Length > 2)
-                 {
-                     Console.WriteLine();
-                     
-                     Console.WriteLine(content.Content);
-                 }
-                 else
-                 {
-                     Console.WriteLine("Received keep-alive message");
-                 }
+                if (content.Content.Length > 2)
+                {
+                    Console.WriteLine();
 
-             })); // register the stream handler
+                    Console.WriteLine(content.Content);
+                }
+                else
+                {
+                    Console.WriteLine("Received keep-alive message");
+                }
+
+            })); // register the stream handler
             Console.WriteLine("Passed stream handler registration");
-            await task;// start the stream                         
-        } 
+            await task;// start the stream    
+        }
     }
 }
