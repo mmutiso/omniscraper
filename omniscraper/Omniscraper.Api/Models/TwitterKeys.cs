906348c601ce2c41ca0ca3cb28b0c9e8a22f8eec
Matthew <mutee88@gmail.com>
2022-01-07 16:33:40 +0300

    Entire functionality to fetch threads

---
+++ omniscraper/Omniscraper.Api/Models/TwitterKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Omniscraper.Api.Models
+{
+    public record TwitterKeys
+    {
+        public string ConsumerKey { get; init; }
+        public string ConsumerSecret { get; init; }
+        public string AccessToken { get; init; }
+        public string AccessTokenSecret { get; init; }
+    }
+}
